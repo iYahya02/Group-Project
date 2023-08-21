eb8188c5a3c360fd365a49de07604a2877b82f4e
Yahya Al Mahrouqi <141571721+iYahya02@users.noreply.github.com>
2023-08-21 19:25:47 +0400

    Add files via upload

---
+++ Group Project/pr/assets/js/main.js
@@ -0,0 +1,9 @@
+// let first = document.getElementById("first");
+// let last = document.getElementById("last");
+// let text = document.getElementById("text");
+
+// window.addEventListener("scroll", () => {
+//   // first.style.top = window.scrollY * 0.7 + "px";
+//   last.style.bottom = window.scrollY * 0.5 + "px";
+//   text.style.top = window.scrollY * 1 + "px";
+// });
